e76ec07bf8d3d520d82894c61ab580e6ac85c049
Matt Parker <mattmparker@gmail.com>
2013-03-06 16:23:17 -0700

    Restructure to load pre-cleaned data.
    Moved all the cleaning elsewhere, so all this script needs to do is pull in
    the good stuff.

---
+++ run_auto_qa.r
@@ -0,0 +1,19 @@
+# This script coordinates the execution of all of the other data QA functions.
+
+
+
+# Strings ain't factors
+options(stringsAsFactors = FALSE)
+
+
+
+# Load the most recent TO 1 data into a list
+load("G:\\StrategicArea\\TB_Program\\Research\\TBESC 2\\Data\\cleaned\\to1_cleaned.rdata")
+
+
+# Set a directory for the output
+outdir <- "G:\\StrategicArea\\TB_Program\\Research\\TBESC 2\\QA\\auto_qa\\problems"
+
+
+# Test-related checks
+tst_check(originals)
