b6b1f4bfac315b5588dd742a658547cab3e8835a
Matt Parker <mattmparker@gmail.com>
2013-06-25 13:49:56 -0600

    Use centralized CSS. Fixes #11.

--- generate_report.r
+++ generate_report.r
@@ -1,6 +1,20 @@
 
 
-library(knitr)
 
-# Convert the report to HTML
-knit2html("qa_report.rmd")
+
+# Strings ain't factors
+options(stringsAsFactors = FALSE)
+
+
+# Load knitr and markdown to generate the reports
+library(knitr)
+library(markdown)
+
+# Knit 
+knit("qa_report.rmd")
+
+markdownToHTML(file = "qa_report.md",
+               output = "qa_report.html",
+               stylesheet = file.path("..", "css", "tbesc_report.css"))
+
+
